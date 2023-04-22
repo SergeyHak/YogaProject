56b39c56087f60c83873e20758cde50b129ff037
SergeyHak <119960518+SergeyHak@users.noreply.github.com>
2023-04-22 23:29:15 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,1 @@
-// import MainPage from "./pages/Main/Main";
-import ProfilePage from "./pages/profile/profile.jsx"
-function App() {
-  return (
-  //  <MainPage/>
-  <ProfilePage/>
-  )
-}
 
-export default App;
