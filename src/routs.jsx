39fa518266a3c810ee57bc42bf18c3e252a08188
irarekut <irarekut@gmail.com>
2023-04-25 17:49:15 +0600

    wip

--- src/routs.jsx
+++ src/routs.jsx
@@ -4,7 +4,9 @@ import Registration from "./pages/authorization/registration";
 import Login from "./pages/authorization/login";
 import MainPage from "./pages/Main/Main";
 import Lesson from "./pages/lesson/Lesson";
-import CoursePage from "./pages/CoursePage/CoursePage";
+import CoursePage from "./pages/coursePage/coursePage";
+import ProfilePage from "./pages/profile/profile";
+import WorkoutPage from "./pages/workoutVideo/workoutVideo";
 
 function AppRoutes() {
   return (
@@ -14,6 +16,8 @@ function AppRoutes() {
       <Route path="/" element={<MainPage />} />
       <Route path="/lesson" element={<Lesson />} />
       <Route path="/courses" element={<CoursePage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/workout" element={<WorkoutPage />} />
     </Routes>
   );
 }
