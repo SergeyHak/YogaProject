58520abf2aab7831a7b2a70a08743e11284f3869
SergeyHak <119960518+SergeyHak@users.noreply.github.com>
2023-04-28 14:31:41 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
   appId: "1:749413357426:web:3cf2f35c820ef6d01befeb",
   measurementId: "G-B2L0P9JB8M",
 };
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-console.log(analytics);
+
 
 
