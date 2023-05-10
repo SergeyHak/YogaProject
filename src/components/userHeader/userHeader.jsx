55596ec57b283ad6f4322edcd5bd5c31bf51a4ad
Georgy Komarov <komarovgosha001@gmail.com>
2023-05-10 17:05:55 +0300

    Сделал единый UserHead

--- src/components/userHeader/userHeader.jsx
+++ src/components/userHeader/userHeader.jsx
@@ -1,21 +1,39 @@
-import * as S from "./styles";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../store/hooks/use-auth";
+
+import * as S from "./styles";
+import * as A from "../../pages/coursePage/styles";
 
 import logoBlack from "../../img/logoBlack.png";
 import UserPhoto from "../../img/EllipsePhoto.png";
 
 export function UserHeader() {
+  const { isAuth } = useAuth();
+
   return (
-    <S.HeadContentDiv>
-      <S.LogoTitleDiv>
-        <Link to='/'>
-          <S.LogoImg src={logoBlack} alt="logo" />
-        </Link>
-      </S.LogoTitleDiv>
-      <S.UserDiv>
-        <S.UserPhotoImg src={UserPhoto} alt="userphoto" />
-        <S.UserNameSpan>Сергей ↓</S.UserNameSpan>
-      </S.UserDiv>
-    </S.HeadContentDiv>
+    <div>
+      {isAuth ? (
+        <S.HeadContentDiv>
+          <S.LogoTitleDiv>
+            <Link to="/">
+              <S.LogoImg src={logoBlack} alt="logo" />
+            </Link>
+          </S.LogoTitleDiv>
+          <S.UserDiv>
+            <S.UserPhotoImg src={UserPhoto} alt="userphoto" />
+            <S.UserNameSpan>Сергей ↓</S.UserNameSpan>
+          </S.UserDiv>
+        </S.HeadContentDiv>
+      ) : (
+        <A.TitleBlock>
+          <Link to="/">
+            <A.LogoImg src={logoBlack} alt="logoBlack" />
+          </Link>
+          <Link to="/login">
+            <A.EnterButton>Войти</A.EnterButton>
+          </Link>
+        </A.TitleBlock>
+      )}
+    </div>
   );
 }
